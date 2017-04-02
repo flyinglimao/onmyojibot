cc6bce3a78e2c5f5db71301edae77e2c726eb09e
Pada <pada@pa-da.me>
2017-04-02 17:48:07 +0800

    chanel list

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.use( bodyParser.urlencoded( {extended: true} ) )
 let wanted = require('./wanted.json')
 let map = require('./map.json')
 let illustration = require('./illustration.json')
+let chanel = `
+章魚單車頻道：4747
+御魂拾層頻道：1010
+碎片挑戰頻道：818 (桃花/鐮鼬)
+覺醒材料頻道：886
+懸賞交換頻道：1700
+`
 
 app.all('/', (req, res) => {
   res.sendStatus(403)
@@ -23,8 +30,9 @@ app.post('/in', (req, res) => {
     data.entry.forEach( (entry) => {
       entry.messaging.forEach( (event) => {
         if ( event.message ) {
-		  console.log(`Got msg: ${event.message.text}`)
-          sendMsg( event.sender.id, event.message.text )
+		  // console.log(`Got msg: ${event.message.text}`)
+		  let reply = looker( event.message.text )
+          sendMsg( event.sender.id, reply )
         } else
 		  console.log(' Error ')
 	  })
@@ -34,7 +42,39 @@ app.post('/in', (req, res) => {
 })
 
 function looker ( input ) {
-  
+  let spilt = input.match(/\S+/)
+  switch ( spilt[0] ){
+    case '幫助':
+	  return help
+	  break
+	case '懸賞':
+	  return wanted[spilt[1]] || '查無資料，請確定目標名稱正確，查詢線索請使用 線索 (條件)'
+	  break
+	case '副本':
+	  return mapLooker(spilt[1])
+	  break
+	case '圖鑑':
+	  return illLooker(spilt[1])
+	  break
+	case '線索':
+	  return wantedLooker(spilt[1])
+	  break
+	case '頻道':
+	  return chanel
+	  break
+	case '留言':
+	  comment(input)
+	  break
+  }
+}
+
+function wantedLooker ( dex ) {
+}
+
+function mapLooker ( dex ) {
+}
+
+function illLooker ( dex ) {
 }
 
 function sendMsg ( sender, message ) {
