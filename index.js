33e198085e2522f3273115db691d2b12eaccd671
pa-da <pa-da@pa-da.me>
2017-04-04 17:21:14 +0800

    persistent menu

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.post('/in', (req, res) => {
   if (data.object === 'page') {
     data.entry.forEach((entry) => {
       entry.messaging.forEach((event) => {
-        if (event.message) {
-          let reply = looker(event.message.text || event.postback.payload || 'Except')
+        if (event.message || event.payload) {
+          let reply = looker(event.message.text || 'Except')
           reply.forEach((msg) => {
             if (typeof (msg) === 'string') {
               sendMsg(event.sender.id, msg)
