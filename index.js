f4d9962b0de132e1684e783c38d9d4b543d6a32c
pada@pa-da.me <pada@pa-da.me>
2017-04-05 20:33:51 +0800

    blur

--- index.js
+++ index.js
@@ -142,10 +142,8 @@ function wantedLooker (dex) {
         })
       }
     }
-    if (dex[0] === '線索') {
-      result = result.concat('查詢結果為: ' + (mat.join(',') || '無資料'))
-      if (mat.length === 1) { result = result.concat(wanted[mat[0]]) }
-    }
+    if (mat.length >= 1 || dex[0] === '線索') { result = result.concat('查詢結果為: ' + (mat.join(',') || '無資料')) }
+    if (mat.length === 1) { result = result.concat(wanted[mat[0]]) }
   }
   return result
 }
