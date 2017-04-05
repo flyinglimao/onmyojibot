77d06bc171b68ed01d86f314dee76afe3d8d8f87
pada@pa-da.me <pada@pa-da.me>
2017-04-05 20:20:26 +0800

    blur

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ function wantedLooker (dex) {
         mat.forEach((sp) => {
           if (!(sp in tmp)) { mat.splice(mat.indexOf(sp), 1) }
         })
-      })
+      }
     }
     result = result.concat('查詢結果為: ' + (mat.join(',') || '無資料'))
     if (mat.length === 1) { result = result.concat(wanted[mat[0]]) }
